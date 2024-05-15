0bbbab80c449a2141ab09ff5482fb5375c0c9e78
Zack <114314332+ZackJohnson1@users.noreply.github.com>
2024-05-14 21:17:15 -0700

    Create input_basics_v2.cpp

---
+++ cpp_basics/input_basics_v2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Note: The angular brackets are opposite
+    double x;
+    double y;
+
+    cout << "Enter the values for x: ";
+    cin >> x;
+
+    cout << "Enter the values for y5: ";
+    cin >> y;
+    
+    cout << "Sum: " << x + y;
+    
+    return 0;
+}
