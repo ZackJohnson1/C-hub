0110a8cb90e811ad5c6ffd699f0f9020048d9ad5
Zack <114314332+ZackJohnson1@users.noreply.github.com>
2024-05-14 21:04:18 -0700

    Create input_basics.cpp

---
+++ cpp_basics/input_basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter a value: ";
+    int val;
+
+    // Note: The angular brackets are opposite 
+    cin >> val;                     
+    cout << "Heres the number you just entered: " << val;
+    return 0;
+}
