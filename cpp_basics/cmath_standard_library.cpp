3d1bc7e1d786c75a638cbe35ac20619d37485a19
Zack <114314332+ZackJohnson1@users.noreply.github.com>
2024-05-14 22:01:04 -0700

    Create cmath_standard_library.cpp

---
+++ cpp_basics/cmath_standard_library.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double result1 = floor(1.2);    // Rounds a number down
+    double result2 = pow(3, 2);     // 3^2=9
+    cout << "Floor example: " << result1;
+    cout << "Pow example: " << result2;
+    return 0;
+}
