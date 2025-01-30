45531c77310e90f1d1b959defc23e2f4c2327683
zachjohnson <johnsza5@oregonstate.edu>
2025-01-30 14:20:47 -0600

    oblig + float

---
+++ hellowrld.c
@@ -0,0 +1,12 @@
+//
+// Created by Zach Johnson on 1/30/25.
+//
+
+#include <stdio.h>
+int main() {
+    char* oblig = "Hello World";
+    float itsOver = 9000.0f;
+    printf("%s\n", oblig);
+    printf("IT\'S OVER %.2f!\n", itsOver);
+    return 0;
+}
